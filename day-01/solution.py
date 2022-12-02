986cdbdcdb1722375b75cfe2e1823b2ee6155f21
Liam Taylor <liam@piclo.energy>
2022-12-02 01:28:32 +0000

    completes day 1 challenge 2

--- day-01/solution.py
+++ day-01/solution.py
@@ -1,15 +1,15 @@
-
 with open('input_data.txt', 'r') as infile:
     input_data: list = infile.read().splitlines()
-    max_elf = 0
+    elves = []
     current_elf = 0
     for datum in input_data:
         if not datum:
-            if current_elf > max_elf:
-                max_elf = current_elf
+            elves.append(current_elf)
             current_elf = 0
             continue
         current = int(datum)
         current_elf += current
+    else:
+        elves.append(current_elf)
 
-print(max_elf)
+print(sum(sorted(elves)[:-4:-1]))
