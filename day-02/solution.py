f0ad40244c9c203b30106f8ef7b1f0c056e56b4c
Liam Taylor <liam@piclo.energy>
2022-12-03 11:24:51 +0000

    completes day 2 task 2

--- day-02/solution.py
+++ day-02/solution.py
@@ -7,18 +7,17 @@ class RPS(Enum):
     PAPER = 'paper'
     SCISSORS = 'scissors'
 
+class Outcomes(Enum):
+
+    LOSE = 'X'
+    DRAW = 'Y'
+    WIN = 'Z'
+
 
 ELF_RPS = {
     'A': RPS.ROCK,
     'B': RPS.PAPER,
     'C': RPS.SCISSORS,
-}
-
-
-HUMAN_RPS = {
-    'X': RPS.ROCK,
-    'Y': RPS.PAPER,
-    'Z': RPS.SCISSORS,
 }
 
 
@@ -29,6 +28,13 @@ WIN_LOGIC = {
 }
 
 
+LOSE_LOGIC = {
+    RPS.SCISSORS: RPS.ROCK,
+    RPS.ROCK: RPS.PAPER,
+    RPS.PAPER: RPS.SCISSORS,
+}
+
+
 RPS_SCORE = {
     RPS.ROCK: 1 ,
     RPS.PAPER: 2,
@@ -36,8 +42,13 @@ RPS_SCORE = {
 }
 
 
-DRAW_SCORE = 3
-WIN_SCORE = 6
+OUTCOME_SCORE = {
+    Outcomes.WIN.value: 6,
+    Outcomes.LOSE.value: 0,
+    Outcomes.DRAW.value: 3,
+
+}
+
 
 with open('input_data.txt', 'r') as infile:
     input_data: list = infile.read().splitlines()
@@ -51,13 +62,20 @@ def evaluate_game(player_choice: RPS, opponent_choice: RPS) -> int:
         outcome_score = 6
     return choice_score + outcome_score
 
+def get_corresponding_choice(opponent_choice: RPS, outcome: Outcomes) -> RPS:
+    if outcome == Outcomes.WIN.value:
+        return LOSE_LOGIC[opponent_choice]
+    elif outcome == Outcomes.LOSE.value:
+        return WIN_LOGIC[opponent_choice]
+    return opponent_choice
+
 tournament_score = 0
 for game in input_data:
-    elf_encoded_choice, human_encoded_choice = game.split(' ')
+    elf_encoded_choice, outcome = game.split(' ')
     elf_choice = ELF_RPS[elf_encoded_choice]
-    human_choice = HUMAN_RPS[human_encoded_choice]
-    game_score = evaluate_game(human_choice, elf_choice)
-    tournament_score += game_score
-
+    outcome_score = OUTCOME_SCORE[outcome]
+    choice = get_corresponding_choice(elf_choice, outcome)
+    choice_score = RPS_SCORE[choice]
+    tournament_score += (outcome_score + choice_score)
 
 print(tournament_score)
