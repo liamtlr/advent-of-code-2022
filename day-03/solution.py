054e3afb948c7fb8bdbae92106d24578c2dec337
Liam Taylor <liam@piclo.energy>
2022-12-05 17:21:56 +0000

    add day-03 content

--- day-03/solution.py
+++ day-03/solution.py
@@ -5,15 +5,13 @@ with open('input_data.txt', 'r') as infile:
     input_data: list = infile.read().splitlines()
 
 score = 0
-for rucksack in input_data:
-    compartment_size = int(len(rucksack) / 2)
-
-    compartment_1, compartment_2 = rucksack[:compartment_size], rucksack[compartment_size:]
-    common = list(set(compartment_1).intersection(compartment_2))[0]
+ELF_GROUP_SIZE = 3
+for rucksack in range(0, len(input_data), 3):
+    elf_1, elf_2, elf_3 = input_data[rucksack:rucksack + ELF_GROUP_SIZE]
+    common = list(set(elf_1).intersection(set(elf_2)).intersection(elf_3))[0]
     letter_index = string.ascii_lowercase.index(common.lower()) + 1
     if common.isupper():
         letter_index = letter_index + len(string.ascii_lowercase)
     score += letter_index
 
 print(score)
-
